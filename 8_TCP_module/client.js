5a1ea133dd14be4047b1d48eaf943c626db246d3
Vasily93 <markovavasily@gmail.com>
2020-08-18 16:13:30 -0700

    learned about how to handle errors and reconnect to sreams

--- 8_TCP_module/client.js
+++ 8_TCP_module/client.js
@@ -4,20 +4,38 @@ const handler = {
     retries: 0,
     maxRetries: 10,
     interval: 5000,
+    quit: false,
+    inputHandler(data) {
+        if(data.toString().trim().toUpperCase() === 'QUIT') {
+            this.quit = true;
+            console.log('QUIT!');
+            this.conn.end();
+            process.stdin.pause();
+        } else {
+            this.conn.write(data)
+        }
+    },
     connect() {
         console.log('connecting...');
         this.conn = net.createConnection(port);
         this.conn.on('close', this.close.bind(this));
         this.conn.on('error', this.error.bind(this));
+        process.stdin.resume()
+        process.stdin.on('data', this.inputHandler.bind(this));
+        this.conn.pipe(process.stdout, {end: false});
     },
     close() {
-        console.log('closing.....')
+        console.log('Connection closed... Trying again');
+        process.stdin.pause();
+        this.reconnect();
     },
-    error() {
-        console.log('oops! something is wrong')
+    error(err) {
+        console.log(`Error: ${err}`);
+        this.reconnect();
     },
     reconnect() {
-
+        console.log('reconnecting...');
+        
     }
 };
 
