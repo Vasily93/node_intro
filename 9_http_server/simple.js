be535785500a4181ad84013ee367c974cb1e8cce
Vasily93 <markovavasily@gmail.com>
2020-08-20 17:04:12 -0700

    changed to simple text

--- 9_http_server/simple.js
+++ 9_http_server/simple.js
@@ -1,9 +1,8 @@
 const http = require("http");
-const fs = require('fs');
+// const fs = require('fs');
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'video/mp4'});
-    const rs = fs.createReadStream('./teleport.mp4');
-    rs.pipe(response);
+    response.writeHead(200, {'Content-Type': 'text/pain'});
+    response.end('Hello there form Node.js!')
 });
 
 server.listen(8080, () => {console.log('listening')})
