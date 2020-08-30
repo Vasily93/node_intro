53d45a2ee87392ea9b8319d46c5c191e236ed0c1
Vasily93 <markovavasily@gmail.com>
2020-08-30 12:07:36 -0700

    connected to twitter api and got some twitts!

--- twit-find/index.js
+++ twit-find/index.js
@@ -1,5 +1,11 @@
 const program = require('commander');
+const Twit = require('twit');
+const options = Object.assign({'timeout_ms': 60 * 1000},
+                                require('./node_modules/credentials.json'))
 const log = console.log;
+
+
+const client = new Twit(options);
 
 program
     .arguments('<query>')
@@ -8,6 +14,14 @@ program
     .parse(process.argv)
 
     function search(query) {
-        log(query);
-        log(program.limit);
+        client.get('search/tweets', 
+                {q: 'Elon Musk'},
+                (err, data, response) => {
+                    if(err) {
+                        throw Error(err);
+                    }
+                    // log(data)
+                    const tweets = data.statuses.map(obj => obj.text);
+                    log(tweets);
+                })    
     }
