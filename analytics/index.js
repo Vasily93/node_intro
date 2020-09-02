8cc1036021a25934e3c753245ba49ce1c305c48a
Vasily93 <markovavasily@gmail.com>
2020-09-01 17:50:06 -0700

    finished click mapper! So fum :)

--- analytics/index.js
+++ analytics/index.js
@@ -1,17 +1,37 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const io = require('socket.io')(http);
+const http = require('http');
+const server = http.createServer(app);
+const socketio = require('socket.io');
 const port = 9090;
 
-console.log(io)
+const io = socketio(server);
+
+const points = [];
 
 app.get('/', (req, res) => {
     const filepath = path.join(__dirname, 'index.html');
     res.sendFile(filepath);
 });
 
-app.listen(port, () => {
+app.get('/analytics', (req, res) => {
+    const filepath = path.join(__dirname, 'analysis.html');
+    res.sendFile(filepath);
+});
+
+io.on('connection', socket => {
+    console.log('connected')
+    socket.on('click', msg => {
+        const {x, y} = msg;
+        points.push({x, y});
+    });
+    socket.on('retrieve', () => {
+        io.emit('analytics', points)
+    })
+});
+
+server.listen(port, () => {
     console.log(`now listening on port ${port}`)
 });
 
